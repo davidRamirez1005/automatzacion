f67676b213e4e6a5cddd9d16a0c3b9d4d965a254
Jose David Ramirez <126982405+davidRamirez1005@users.noreply.github.com>
2024-07-15 08:56:01 -0500

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
-
 # Función para crear elementos
 create_new_item() {
   read -p "Nombre de la ruta: " routeName
   read -p "Nombre del controlador: " controllerName
   read -p "Nombre de la carpeta del controlador (dentro de script/controller/): " controllerFolderName
-  read -p "Nombre del servicio: " serviceName
-  read -p "Ruta del archivo de servicio (relativa a script/controller/service/): " serviceFilePathRel
   read -p "Nombre de la carpeta del formulario: " formFolderName
-
-  projectRoot=$(dirname "$0")
-
+  projectRoot=$(dirname "$0")/../..
+  # Cambiar al directorio raíz del proyecto
+  cd "$projectRoot" || exit
   # Crear ruta en ruta.js (añadiendo al final)
-  routeFilePath="$projectRoot/script/ruta.js"
-  echo -e "\nconsole.log('Ruta seleccionada: $routeName');" >> "$routeFilePath"
-
+  routeFilePath="script/rutas.js"
+  routeConfig="\$routeProvider.when('/$routeName', {
+        templateUrl: 'form/$formFolderName/$routeName.html?i=' + Math.random().toString(36).slice(2),
+        controller: '${controllerName}Controller',
+        resolve: {
+            lazy: ['$ocLazyLoad', function (\$ocLazyLoad) {
+                return \$ocLazyLoad.load([{
+                    name: APPNAME,
+                    files: ['script/controller/$controllerFolderName/$controllerName.js?i=' + Math.random().toString(36).slice(2)]
+                }]);
+            }]
+        }
+    });"
+  echo -e "\n$routeConfig" >> "$routeFilePath"
   # Crear controlador en la carpeta controller
-  controllerDir="$projectRoot/script/controller/$controllerFolderName"
+  controllerDir="script/controller/$controllerFolderName"
   mkdir -p "$controllerDir"
   controllerFilePath="$controllerDir/$controllerName.js"
-  echo -e "// Controlador: $controllerName" > "$controllerFilePath"
-
-  # Crear servicio en la carpeta service
-  # serviceDir="$projectRoot/script/controller/service"
-  # mkdir -p "$serviceDir"
-  # serviceFile="$serviceDir/$serviceName.js"
-  # serviceFilePath="$serviceDir/$serviceFilePathRel"
-  # echo -e "console.log('servicio');" > "$serviceFilePath"
-
+  controllerCode="angular.module(APPNAME).controller('${controllerName}Controller', function (\$scope, configuracionGlobal, \$rootScope, servicioGeneral, servicioConfiguracion, servicioAdministracion) {
+});"
+  echo -e "$controllerCode" > "$controllerFilePath"
   # Crear archivo HTML en la carpeta especificada de form
-  formDir="$projectRoot/form/$formFolderName"
+  formDir="form/$formFolderName"
   mkdir -p "$formDir"
   formFilePath="$formDir/$routeName.html"
   echo -e "<!-- HTML para $routeName -->" > "$formFilePath"
-
   echo "Elementos creados exitosamente"
 }
-
 # Llamar a la función para crear elementos
 create_new_item
