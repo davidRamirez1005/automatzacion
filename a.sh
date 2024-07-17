f55a5cd5fdda64f089b3a58b249a2b54d7f45840
Jose David Ramirez <126982405+davidRamirez1005@users.noreply.github.com>
2024-07-17 11:33:41 -0500

    Update a.sh

--- a.sh
+++ a.sh
@@ -8,26 +8,38 @@ create_new_item() {
   projectRoot=$(dirname "$0")/../..
   # Cambiar al directorio raíz del proyecto
   cd "$projectRoot" || exit
-  # Crear ruta en ruta.js (añadiendo al final)
+  # Crear ruta en ruta.js (añadiendo antes de otherwise)
   routeFilePath="script/rutas.js"
-  routeConfig="\$routeProvider.when('/$routeName', {
-        templateUrl: 'form/$formFolderName/$routeName.html?i=' + Math.random().toString(36).slice(2),
-        controller: '${controllerName}Controller',
-        resolve: {
-            lazy: ['$ocLazyLoad', function (\$ocLazyLoad) {
-                return \$ocLazyLoad.load([{
-                    name: APPNAME,
-                    files: ['script/controller/$controllerFolderName/$controllerName.js?i=' + Math.random().toString(36).slice(2)]
-                }]);
-            }]
-        }
-    });"
-  echo -e "\n$routeConfig" >> "$routeFilePath"
+  routeConfig="    \$routeProvider.when('/$routeName', {\n\
+        templateUrl: 'form/$formFolderName/$routeName.html?i=' + Math.random().toString(36).slice(2),\n\
+        controller: '${controllerName}Controller',\n\
+        resolve: {\n\
+            lazy: ['$ocLazyLoad', function (\$ocLazyLoad) {\n\
+                return \$ocLazyLoad.load([{\n\
+                    name: APPNAME,\n\
+                    files: ['script/controller/$controllerFolderName/$controllerName.js?i=' + Math.random().toString(36).slice(2)]\n\
+                }]);\n\
+            }]\n\
+        }\n\
+    });\n"
+  # Usar awk para insertar el nuevo código de ruta antes de otherwise
+  awk -v routeConfig="$routeConfig" '
+  BEGIN { 
+    inserted = 0 
+  }
+  {
+    if (!inserted && $0 ~ /\$routeProvider\.otherwise\({/) {
+      print routeConfig
+      inserted = 1
+    }
+    print
+  }
+  ' "$routeFilePath" > "$routeFilePath.tmp" && mv "$routeFilePath.tmp" "$routeFilePath"
   # Crear controlador en la carpeta controller
   controllerDir="script/controller/$controllerFolderName"
   mkdir -p "$controllerDir"
   controllerFilePath="$controllerDir/$controllerName.js"
-  controllerCode="angular.module(APPNAME).controller('${controllerName}Controller', function (\$scope, configuracionGlobal, \$rootScope, servicioGeneral, servicioConfiguracion, servicioAdministracion) {
+  controllerCode="angular.module(APPNAME).controller('${controllerName}Controller', function (\$scope, configuracionGlobal, \$rootScope, servicioGeneral, servicioConfiguracion, servicioAdministracion) {\n\
 });"
   echo -e "$controllerCode" > "$controllerFilePath"
   # Crear archivo HTML en la carpeta especificada de form
